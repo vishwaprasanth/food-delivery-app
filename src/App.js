d8303fc9094beb262e9d6c4929451d34c73d2ea3
Vishwa prasanth <vishwaprasanth1411@gmail.com>
2023-06-20 15:14:01 +0530

    Navbar-completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
-      <h1>hhhh</h1>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
